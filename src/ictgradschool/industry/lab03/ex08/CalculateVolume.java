89acc5cebc44a957193ffa8305c42a12252cc638
pqsky7 <qpen546@aucklanduni.ac.nz>
2017-03-13 15:48:47 +1300

    lab 03

---
+++ src/ictgradschool/industry/lab03/ex08/CalculateVolume.java
@@ -0,0 +1,29 @@
+package ictgradschool.industry.lab03.ex08;
+
+import ictgradschool.Keyboard;
+
+/*
+ * Created by qpen546 on 13/03/2017.
+ */
+public class CalculateVolume {
+    private void start() {
+
+        int radius;
+
+        System.out.println("\"Volume of a Sphere\"");
+
+        System.out.print("Enter the radius: ");
+
+        radius = (int)Double.parseDouble(Keyboard.readInput());
+
+        double volume = 4 / 3 * Math.PI * Math.pow(radius,2);
+
+        System.out.println("Volume: " + volume);
+
+    }
+
+    public static void main(String[] args){
+        CalculateVolume p = new CalculateVolume();
+        p.start();
+    }
+}
